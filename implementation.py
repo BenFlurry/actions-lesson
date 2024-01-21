fb95c200378e4b1dbfb0cd8ff8cc3ab0dac4236d
= <maulik.tripathi.23@ucl.ac.uk>
2024-01-21 23:47:51 +0000

    edit

--- implementation.py
+++ implementation.py
@@ -7,13 +7,31 @@ class TreeImplementation(Tree):
         Insert a value into the tree. Does nothing if the value is already in
         the tree.
         """
-        pass
+        if value < self.value:
+            if self.left == None:
+                self.left = Tree(value)
+            else:
+                self.insert(self.left, value)
+        else:
+            if self.right == None:
+                self.right = Tree(value)
+            else:
+                self.insert(self.right, value)
 
     def contains(self, value: int) -> bool:
         """
         Check if the tree contains a value.
         """
-        return False
+        if value == self.value:
+            return True
+        elif value > self.value:
+            if self.right == None:
+                return False
+            return self.contains(self.right, value)
+        else:
+            if self.left == None:
+                return False
+            return self.contains(self.left, value)  
 
     def remove(self, value: int) -> None:
         """
@@ -21,22 +39,58 @@ class TreeImplementation(Tree):
         in the tree, or a ValueError if the removal would result in an empty
         tree.
         """
-        pass
+        if value == self.value:
+            if self.left == None and self.right == None:
+                raise ValueError("Cannot remove root from tree with no children")
+            elif self.left == None:
+                self.value = self.right.value
+                self.left = self.right.left
+                self.right = self.right.right
+            elif self.right == None:
+                self.value = self.left.value
+                self.right = self.left.right
+                self.left = self.left.left
+            else:
+                self.value = self.left.max_value()
+                self.left.remove(self.value)
+        elif value > self.value:
+            if self.right == None:
+                raise KeyError("Value not in tree")
+            self.right.remove(value)
+        else:
+            if self.left == None:
+                raise KeyError("Value not in tree")
+            self.left.remove(value)
 
     def min_value(self) -> int:
         """
         Return the minimum value in the tree.
         """
-        return 0
-
+        if self.left == None:
+            return self.value
+        return self.left.min_value()
+    
     def max_value(self) -> int:
         """
         Return the maximum value in the tree.
         """
-        return 0
+        if self.right == None:
+            return self.value
+        return self.right.max_value()
 
     def inorder_traversal(self) -> list[int]:
         """
         Return the inorder traversal (left, root, right) of the tree as a list.
         """
-        return []
+
+        left = []
+        mid = [self.value]
+        right = []
+        
+        if self.left != None:
+            left = self.inorder_traversal(self.left)
+        if right != None:
+            right = self.inorder_traversal(self.right)
+
+
+        return left.append(mid) + right 
